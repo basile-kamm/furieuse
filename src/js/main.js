c089da29e6bc4b929004ec8fe1dec98a6c84c992
basile-kamm <another-black-kitty@proton.me>
2024-11-12 14:31:26 +0100

    Html css

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,12 @@ window.addEventListener("scroll", () => {
   }
   lastScrollPosition = currentScrollPosition;
 });
+
+// Set banner height from children
+const banner = document.querySelector(".banner");
+// Find the tallest image height
+const tallestImage = Math.max(
+  ...Array.from(banner.querySelectorAll("img")).map((img) => img.height)
+);
+// Set the banner height to match the tallest image
+banner.style.height = `${tallestImage}px`;
