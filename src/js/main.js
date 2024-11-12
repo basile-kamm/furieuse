7bce774fd65a411af68684f3f11753daa34a1be3
basile-kamm <another-black-kitty@proton.me>
2024-11-12 17:15:00 +0100

    oe oe le soir

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,128 @@
 import VanillaTilt from "vanilla-tilt";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-// Header Animation
-const header = document.querySelector("header");
+gsap.registerPlugin(ScrollTrigger);
 
-let lastScrollPosition = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  // Header Animation
+  const header = document.querySelector("header");
 
-window.addEventListener("scroll", () => {
-  let currentScrollPosition = window.scrollY;
+  let lastScrollPosition = 0;
 
-  if (currentScrollPosition > lastScrollPosition) {
-    header.classList.add("scrolling-down");
-  } else {
-    header.classList.remove("scrolling-down");
+  window.addEventListener("scroll", () => {
+    let currentScrollPosition = window.scrollY;
+
+    if (currentScrollPosition > lastScrollPosition) {
+      header.classList.add("scrolling-down");
+    } else {
+      header.classList.remove("scrolling-down");
+    }
+    lastScrollPosition = currentScrollPosition;
+  });
+
+  // Set banner height from children
+
+  function findTallestImg() {
+    const banner = document.querySelector(".banner");
+    const tallestImage = Math.max(
+      ...Array.from(banner.querySelectorAll("img")).map((img) => img.height) // Find the tallest image height
+    );
+
+    banner.style.height = `${tallestImage}px`; // Set the banner height to match the tallest image
   }
-  lastScrollPosition = currentScrollPosition;
-});
+  window.addEventListener("resize", findTallestImg);
+  findTallestImg();
 
-// Set banner height from children
-const banner = document.querySelector(".banner");
-// Find the tallest image height
-const tallestImage = Math.max(
-  ...Array.from(banner.querySelectorAll("img")).map((img) => img.height)
-);
-// Set the banner height to match the tallest image
-banner.style.height = `${tallestImage}px`;
+  // Parallax effect
+  const container = document.querySelector(".hero");
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: container,
+      markers: true,
+      start: "top top",
+      end: "bottom top",
+      scrub: true,
+    },
+  });
+
+  tl.to("#hero-1", {
+    y: -400,
+    scale: 1.1,
+  });
+
+  tl.to(
+    "#hero-2",
+    {
+      y: -200,
+    },
+    "<"
+  );
+  tl.to(
+    "#hero-3",
+    {
+      y: 50,
+    },
+    "<"
+  );
+  tl.to(
+    "#hero-ysa",
+    {
+      y: -150,
+      x: -200,
+      scale: 1.1,
+    },
+    "<"
+  );
+  tl.to(
+    "#hero-baron",
+    {
+      y: -150,
+      x: 100,
+      scale: 1.1,
+    },
+    "<"
+  );
+  tl.to(
+    "#hero-claude",
+    {
+      y: -50,
+      x: 300,
+      scale: 1.1,
+    },
+    "<"
+  );
+  tl.to(
+    "#hero-roi",
+    {
+      y: -50,
+      x: -300,
+      scale: 1.1,
+    },
+    "<"
+  );
+  tl.to(
+    "#hero-logo",
+    {
+      y: 150,
+      scale: 1.2,
+    },
+    "<"
+  );
+
+  // Shop section anim
+  const shopElements = document.querySelectorAll(".container .row ");
+
+  shopElements.forEach((shopElement) => {
+    gsap.from(shopElement, {
+      scrollTrigger: {
+        trigger: shopElement,
+        start: "-400px 90%",
+        markers: true,
+      },
+      y: 400,
+      stagger: 0.3,
+    });
+  });
+});
